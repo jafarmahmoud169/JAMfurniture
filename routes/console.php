1143cfca3d691f01a2a79557d09d84c15d9fdb6b
jafarmahmoud169 <jafarmahmoud535@gmail.com>
2025-06-03 21:20:47 +0300

    modified:   routes/console.php

--- routes/console.php
+++ routes/console.php
@@ -22,3 +22,23 @@ Artisan::command('serve_me_daddy',function () {
     $this->comment('you want it , you got it');
     artisan::call('serve');
 });
+
+Artisan::command('stop_serve',function () {
+    $this->comment('Stopping server...');
+    if (PHP_OS_FAMILY === 'Windows') {
+        exec('taskkill /F /IM php.exe');
+    } elseif (PHP_OS_FAMILY === 'Linux' || PHP_OS_FAMILY === 'Darwin') {
+        exec('pkill -f "php artisan serve"');
+    } elseif (PHP_OS_FAMILY === 'Android') {
+        exec('ps | grep "php artisan serve" | awk \'{print $2}\' | xargs kill');
+    } else {
+        $this->error('Unsupported operating system');
+        return;
+    }
+    $this->info('Server stopped successfully!');
+});
+
+
+
+
+
