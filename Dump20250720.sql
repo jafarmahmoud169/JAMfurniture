a8ee620400e88e391a9f0df5597723151156607d
jafarmahmoud169 <jafarmahmoud535@gmail.com>
2025-07-20 14:35:16 +0300

    modified:   Dump20250720.sql

--- Dump20250720.sql
+++ Dump20250720.sql
@@ -1,5 +1,5 @@
 CREATE DATABASE IF NOT EXISTS `jamfurniture`
-/*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */
+/*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci */
 /*!80016 DEFAULT ENCRYPTION='N' */
 ;
 USE `jamfurniture`;
